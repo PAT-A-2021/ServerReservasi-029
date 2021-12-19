627758b5fefa73c9427ef84157ad86aa0db592ae
Harry Nur <harry.nur.ft19@mail.umy.ac.id>
2021-12-20 00:58:34 +0700

    Add project files.

---
+++ ServerReservasi-029/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServerReservasi_029.ServiceReference1;
+using ServiceReservasi_029;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerReservasi_029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Service1));
+                hostObj.Open();
+                Console.WriteLine("It's showtime!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
